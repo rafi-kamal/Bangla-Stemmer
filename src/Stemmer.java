e31f9220ae83471d8c1190ba5a0fa645e4a5473f
Rafi Kamal <rafikamal93@gmail.com>
2014-09-14 13:52:55 +0600

    initial commit

---
+++ src/Stemmer.java
@@ -0,0 +1,11 @@
+
+public class Stemmer {
+	/**
+	 * The program will be run using the following command:<p>
+	 * {@code Stemmer -r rulesfile inputfile}.
+	 */
+	public static void main(String[] args) {
+		String rulesFileName = args[2];
+		String inputFileName = args[3];
+	}
+}
